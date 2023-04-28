2360e95330dbda983939072feb751f8304e916f1
Aleksandr Kravchenko <krav559108@gmail.com>
2023-04-28 14:55:37 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,7 @@ export default {
 body {
   margin: 0;
   padding: 0;
-
-  padding-top: constant(safe-area-inset-top); /* iOS 11.0 */
-  padding-top: env(safe-area-inset-top); /* iOS 11.2 */
+  padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
 
 
 }
